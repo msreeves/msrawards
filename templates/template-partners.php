b33c4b577be635a068c9d5cfc631a0dcd6e1b312
msreeves <reevesy87@hotmail.co.uk>
2023-12-23 10:16:36 +0000

    version 1.0

--- templates/template-partners.php
+++ templates/template-partners.php
@@ -26,7 +26,7 @@ get_header();
       ?>
 
       <?php if ( $all_partners->have_posts() ) : ?>
-             <div class="d-flex justify-content-around">
+             <div class="row">
           <?php while ( $all_partners->have_posts() ) : $all_partners->the_post(); ?>	
       <?php get_template_part( 'templates/partials/post-listing/listing-partner' ); ?>
           <?php endwhile; ?>
